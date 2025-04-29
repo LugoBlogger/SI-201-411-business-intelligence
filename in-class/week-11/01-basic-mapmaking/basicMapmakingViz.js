68baa1ca4b8bdf40fcd2a1a6b6fc9af2a6554036
LugoBlogger <lugoblogger@gmail.com>
2025-04-29 15:07:01 +0800

    mercator projection

--- in-class/week-11/01-basic-mapmaking/basicMapmakingViz.js
+++ in-class/week-11/01-basic-mapmaking/basicMapmakingViz.js
@@ -7,7 +7,32 @@ function createBasicMapViz() {
 }
 
 function overallViz(incomingData) {
-  console.log(incomingData);
+  // console.log(incomingData);
+  let countries = incomingData[0];
+  let cities = incomingData[1];
+  // console.log(countries)
+  // console.log(cities)
+
+  let projection = d3.geoMercator()
+    .scale(80).translate([250, 250])
+  let geoPath = d3.geoPath().projection(projection);
+
+  // -- scaling color
+  let featureSize = d3.extent(countries.features, 
+    d => geoPath.area(d))
+  let countryColor = d3.scaleQuantize()
+    .domain(featureSize).range(colorbrewer.Reds[7])
+
+  d3.select("svg").selectAll("path")
+    .data(countries.features)
+    .join("path")
+    .attr("d", geoPath)
+    .attr("class", "countries")
+    .style("fill", d => countryColor(geoPath.area(d)))
+    .style("stroke", d => d3.rgb(countryColor(geoPath(d))));
+  
+
+
 }
 
 function errorHandling(err) {
