199be85d903d49f91557a0a3cc872c85bac48d1e
LugoBlogger <lugoblogger@gmail.com>
2025-06-02 21:03:06 +0800

    C2Live -> C2Live-copy

---
+++ in-class/week-15/C2Live-copy/README.md
@@ -0,0 +1,63 @@
+# C2Live
+
+
+C2Live is an open-source project aimed at providing a comprehensive and interactive platform for tracking C2 servers, tools, and botnets malicious IP addresses over time. This project focuses on categorizing and visualizing these IPs based on the framework they are associated with and the country they originate from. The goal is to help security professionals, researchers, and organizations gain insights into the evolving landscape of cyber threats. This project is based on [C2Tracker](https://github.com/montysecurity/C2-Tracker) from [@_montysecurity](https://twitter.com/_montysecurity).
+
+
+Provided by [@Y_NeXRo](https://twitter.com/Y_NeXRo) and [ikuroNoriiwa](https://github.com/ikuroNoriiwa)  
+
+
+![alt text](https://github.com/YoNixNeXRo/C2Live/blob/main/preview.jpg?raw=true)
+
+The website version at [c2tracker.com](https://c2tracker.com)
+
+## To run the project:
+### Install requirements.txt
+`pip3 install -r requirements.txt`
+### lunch the docker compose
+> Note: Make sure to have docker compose installed :)
+
+
+`docker-compose -f elastic-grafana-docker-compose.yaml up`
+### lunch the connectors.py 
+`python3 connectors.py`  
+It will create geoip pipeline,elastic connector to grafana and import a default dashboard.
+### lunch main.py
+#### Todays datas
+`python3 main.py -u http://localhost:9200/  `  
+It will ingest todays data so you will only have 1 day of data. 
+#### Past datas
+You can also ingest past datas  
+`python3 main.py -u http://localhost:9200/ -n <number_of_history_commits>`  
+> Note: number of history commits is normally equivalent of 1 day. So ingesting 10 history commits will ingest past 10 days datas.
+
+ 
+You can enjoy grafana dashboard on `http://localhost:3000/ `  
+creds are admin:admin
+
+### main.py Usage 
+```
+usage: C2Live Injector [-h] --elastic-url ELASTIC_URL [--elastic-index ELASTIC_INDEX] [--elastic-verify ELASTIC_VERIFY] [--data-url DATA_URL] [--local-path LOCAL_PATH] [--log-level LOG_LEVEL] [--days DAYS]
+
+Ingest C2 data
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --elastic-url ELASTIC_URL, -u ELASTIC_URL
+                        elasticsearch url
+  --elastic-index ELASTIC_INDEX, -i ELASTIC_INDEX
+                        elasticsearch index
+  --elastic-verify ELASTIC_VERIFY, -ev ELASTIC_VERIFY
+                        elasticsearch verify URL
+  --data-url DATA_URL, -d DATA_URL
+                        Data source github repository
+  --local-path LOCAL_PATH, -l LOCAL_PATH
+                        Local path
+  --log-level LOG_LEVEL, -ll LOG_LEVEL
+                        Log Level
+  --days DAYS, -n DAYS  Number of history commits from source url 
+
+```
+### make a cron with main.py to ingest data daily
+
+
---
+++ in-class/week-15/C2Live-copy/main.py
@@ -0,0 +1,124 @@
+from git import Repo
+from os import walk, getcwd, chmod, remove, rmdir, getenv
+from os.path import join, exists
+from elasticsearch import Elasticsearch
+from stat import S_IWUSR
+from datetime import datetime
+from uuid import uuid4
+from argparse import ArgumentParser
+from logging import basicConfig, INFO, getLogger
+
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+UNDERLINE = "\033[4m"
+RESET = "\033[0m"
+
+
+def clone_repo(repo_url: str, dest):
+    getLogger(__name__).info(clone_repo.__name__)
+    getLogger(__name__).debug(f"repo_url : {repo_url}\ndest: {dest}")
+    repo = Repo.clone_from(repo_url, dest)
+    return repo
+
+
+def get_repo_history(repo, days):
+    getLogger(__name__).info(loop_on_data.__name__)
+    getLogger(__name__).debug(f"repo : {repo}")
+    list_commit = repo.iter_commits('main', max_count=days)
+    for commit in list_commit:
+        getLogger(__name__).info(f"commit ID : {commit} - commit message : {repr(commit.message)} - "
+                                 f"commit date : {commit.committed_datetime}")
+        if commit.message == "Nightly Auto Update\n":
+            repo.git.checkout(commit)
+            commit_data = loop_on_data(join(config.local_path, "data"), commit.committed_datetime)
+            insert_data(commit_data, Elasticsearch([config.elastic_url], verify_certs=config.elastic_verify),
+                        config.elastic_index)
+
+
+def loop_on_data(path, date=None) -> list:
+    getLogger(__name__).info(loop_on_data.__name__)
+    getLogger(__name__).debug(f"path : {path}")
+    data = []
+    if date is None:
+        date = datetime.now()
+    for root, dirs, files in walk(path):
+        for file in files:
+
+            if file != "all.txt":
+
+                for ip in open(join(root, file), "r"):
+                    name = file.replace("IPs.txt", "").rstrip()
+                    ip = ip.replace("\n", "").replace("\r", "")
+
+                    # data.append({"framework": name, "ip": ip, "@timestamp": datetime.strftime(
+                    #     date, "%Y-%m-%dT00:00:00+02:00")})
+                    # data.append({"framework": name, "ip": ip, 
+                    #     "@timestamp": datetime.strftime(date, "%Y-%m-%dT00:00:00+08:00")})
+                    data.append({"framework": name, "ip": ip, 
+                        "@timestamp": datetime.utcnow().isoformat()})
+    return data 
+
+
+def insert_data(data, es, index):
+    getLogger(__name__).info(insert_data.__name__)
+    getLogger(__name__).debug(f"data : NotPrinted\nes: {es}\nindex : {index}")
+    for d in data:
+        req = es.create(index=index, id=uuid4().__str__(), document=d, pipeline="geoip")
+
+
+def rmtree(top):
+    getLogger(__name__).info(rmtree.__name__)
+    getLogger(__name__).debug(f"top : {top}")
+    for root, dirs, files in walk(top, topdown=False):
+        for name in files:
+            filename = join(root, name)
+            chmod(filename, S_IWUSR)
+            remove(filename)
+        for name in dirs:
+            rmdir(join(root, name))
+    rmdir(top)
+
+
+def get_config():
+    getLogger(__name__).info(get_config.__name__)
+    parser = ArgumentParser(
+        prog="C2Live Injector",
+        description="Ingest C2 data"
+    )
+    parser.add_argument("--elastic-url", "-u", type=str, help="elasticsearch url", required=True)
+    parser.add_argument("--elastic-index", "-i", type=str.lower, default="c20", help="elasticsearch index")
+    parser.add_argument("--elastic-verify", "-ev", type=bool, default=False, help="elasticsearch verify URL")
+    parser.add_argument("--data-url", "-d", type=str, default="https://github.com/montysecurity/C2-Tracker.git",
+                        help="Data source github repository")
+    parser.add_argument("--local-path", "-l", type=str, default=join(getcwd(), "data"), help="Local path")
+    parser.add_argument("--log-level", "-ll", type=str, default=None, help="Log Level")
+    parser.add_argument("--days", "-n", type=int, default=0, help="Number of history commits from source url ")
+
+    args = parser.parse_args()
+    return args
+
+
+def init_logging():
+    basicConfig(level=INFO, format=f"[{BLUE}%(levelname)s{RESET}] - [{YELLOW}%(asctime)s{RESET}] - [{GREEN}%(name)s{RESET}] - %(message)s")
+    return getLogger(__name__)
+
+
+if __name__ == '__main__':
+    init_logging()
+    config = get_config()
+    if exists(config.local_path):
+        rmtree(config.local_path)
+    r = clone_repo(config.data_url, config.local_path)
+
+    if config.days > 0:
+        get_repo_history(r, config.days)
+    elif config.days == 0:
+        all_data = loop_on_data(join(config.local_path, "data"))
+        insert_data(all_data, Elasticsearch([config.elastic_url], verify_certs=config.elastic_verify), config.elastic_index)
+    rmtree(config.local_path)
